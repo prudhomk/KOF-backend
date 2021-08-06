48e6491c5b9c1b4a850f1da4ede6b6b59f9a5f0e
Kalan Prudhomme <cleaver9@gmail.com>
2021-08-06 07:59:55 -0700

    wrote model, controller for get by id, all current tests pass

--- __tests__/fighter.test.js
+++ __tests__/fighter.test.js
@@ -37,4 +37,63 @@ describe('demo routes', () => {
       quote: 'Come on! I\'m gonna cure you of the disease called life.'
     });
   });
+
+  it('finds all fighters via GET', async () => {
+
+    const karate = await Fighter.insert({
+      name: 'Mr. Karate',
+      japanese: 'Mr.カラテ',
+      origin: 'Art of Fighting',
+      birthplace: 'Japan',
+      style: 'Kyokugenryu Karate',
+      job: 'The True Master of Kyokugenryu Karate',
+      powers: 'Ki',
+      image: '/MrKarate.webp',
+      quote: 'Come on! I\'m gonna cure you of the disease called life.'
+    });
+
+    const goro = await Fighter.insert({
+      name: 'Goro Daimon',
+      japanese: '大門五郎',
+      origin: 'KOF ‘94',
+      birthplace: 'Japan',
+      style: 'Judo',
+      powers: 'Geokinesis',
+      job: 'Judo Master',
+      image: '/gorodaimon.webp',
+      quote: 'Weakness is strength! Let not my appearance fool you!'
+    });
+
+    const angel = await Fighter.insert({
+      name: 'Angel',
+      japanese: 'アンヘル',
+      origin: 'KOF 2001',
+      birthplace: 'Mexico',
+      style: 'Perfunctory, Pro wrestling',
+      powers: 'Enhanced Superhuman',
+      job: 'Agent of NESTS',
+      image: '/Angel.webp',
+      quote: 'Be me good or be me bad, it is I who always prevails.'
+    });
+
+    const res = await request(app).get('/api/v1/fighters');
+    expect(res.body).toEqual([karate, goro, angel]);
+  });
+
+  it('finds a fighter by id via GET', async () => {
+    const fighter = await Fighter.insert({
+      name: 'Angel',
+      japanese: 'アンヘル',
+      origin: 'KOF 2001',
+      birthplace: 'Mexico',
+      style: 'Perfunctory, Pro wrestling',
+      powers: 'Enhanced Superhuman',
+      job: 'Agent of NESTS',
+      image: '/Angel.webp',
+      quote: 'Be me good or be me bad, it is I who always prevails.'
+    });
+
+    const res = await request(app).get(`/api/v1/fighters/${fighter.id}`);
+    expect(res.body).toEqual(fighter);
+  });
 });
