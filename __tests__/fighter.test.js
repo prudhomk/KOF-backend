72d6db9e209cf00f8ed6501ff6e8507c264bbcd5
Kalan Prudhomme <cleaver9@gmail.com>
2021-08-05 17:42:01 -0700

    wrote test for getAll

---
+++ __tests__/fighter.test.js
@@ -0,0 +1,40 @@
+import pool from '../lib/utils/pool.js';
+import setup from '../data/setup.js';
+import request from 'supertest';
+import app from '../lib/app.js';
+import Fighter from '../lib/models/Fighter.js';
+
+describe('demo routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('creates a fighter via POST', async () => {
+    const res = await request(app)
+      .post('/api/v1/fighters')
+      .send({
+        name: 'Mr. Karate',
+        japanese: 'Mr.カラテ',
+        originalAppearance: 'Art of Fighting',
+        birthplace: 'Japan',
+        fightingStyle: 'Kyokugenryu Karate',
+        job: 'The True Master of Kyokugenryu Karate',
+        powers: 'Ki',
+        image: '/MrKarate.webp',
+        quote: 'Come on! I\'m gonna cure you of the disease called life.'
+      });
+
+    expect(res.body).toEqual({
+      id: '1',
+      name: 'Mr. Karate',
+      japanese: 'Mr.カラテ',
+      originalAppearance: 'Art of Fighting',
+      birthplace: 'Japan',
+      fightingStyle: 'Kyokugenryu Karate',
+      job: 'The True Master of Kyokugenryu Karate',
+      powers: 'Ki',
+      image: '/MrKarate.webp',
+      quote: 'Come on! I\'m gonna cure you of the disease called life.'
+    });
+  });
+});
